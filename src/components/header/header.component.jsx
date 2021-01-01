c00b3753b2bf1290dd3fa0516704c062f5f1b956
Frantiska Jelinkova <frantiska.jelinkova@wundermanthompson.com>
2021-01-01 15:50:13 +0100

    integrated firebase into application, created sign in page with sign in and google sign in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -13,6 +14,12 @@ const Header = () => (
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='contact' to='/shop'>CONTACT</Link>
         </div>
+        {
+            currentUser ?
+            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+            : 
+            <link className='option' to='/signin'>SIGN IN</link>
+        }
     </div>
 )
 
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCFexBz4My-utxbpdxHjMRq8w_mIn27VHI",
+    authDomain: "crwn-db-b290a.firebaseapp.com",
+    projectId: "crwn-db-b290a",
+    storageBucket: "crwn-db-b290a.appspot.com",
+    messagingSenderId: "1034032255817",
+    appId: "1:1034032255817:web:8a93b544c77a4e13d60098",
+    measurementId: "G-ZDX1CRGHY5"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
